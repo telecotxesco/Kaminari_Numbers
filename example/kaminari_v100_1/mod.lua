faee79c043430eadce476f0fcd6ca24fe2bb1d46
Telecotxesco <37375334+telecotxesco@users.noreply.github.com>
2020-12-12 02:18:12 +0100

    Add files via upload

---
+++ example/kaminari_v100_1/mod.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = "Kaminari V100",
+		description = "Kaminari Numbers Example",
+		tags = { "Europe", "Locomotive", },
+		authors = { },
+		dependencies = { },
+		url = { "" },
+	},	
+	
+	options = {
+	},
+
+	runFn = function (settings)
+		
+	end
+}
+end
